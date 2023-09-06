690ecc86de4576f0df241863d4a1ccaa8be4871c
Antimony8 <97124775+Antimony8@users.noreply.github.com>
2023-09-06 12:44:05 -0400

    Create test.java

---
+++ test.java
@@ -0,0 +1,16 @@
+    public static int simpleArraySum(List<Integer> ar) {
+        // Initialize a variable to store the sum
+        int sum = 0;
+
+
+        // Iterate through the array and add each element to the sum
+        for (int num : ar) {
+            sum += num;
+        }
+
+
+        return sum;
+    }
+
+
+}
